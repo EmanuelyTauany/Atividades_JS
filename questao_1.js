ec56cacb06b2905398302e74606fbea36a84039e
Emanuely Tauany <102989482+EmanuelyTauany@users.noreply.github.com>
2025-06-03 20:15:34 +0000

    Commit 2: ainda não funcional

--- questao_1.js
+++ questao_1.js
@@ -1,6 +1,42 @@
-function MAIOR_MENOR(a, b, c, d){
+const a=document.getElementById("a").value;
+const b=document.getElementById("b").value;
+const c = document.getElementById("c").value;
+const d = document.getElementById("d").value;
+const e = document.getElementById("e").value;
 
 
+function MAIOR_MENOR(a, b, c, d, e){
+document.getElementById().addEventListener("submit"); 
+
+
+ const maior = a;
+ const menor = 0;
+
+ if(b>maior){
+   maior=b 
+ }else{
+    menor = b;
+   }
+ 
+
+ if(c>maior){
+    maior=c
+ }else{
+    menor = c;
+ }
+ 
+
+ if(d>maior){
+    maior = d;
+ }else{
+    menor = d;
+ }
+ 
+ if(e>maior){
+  maior = e;
+ }else{
+    menor = e;
+ }
 
 
     
